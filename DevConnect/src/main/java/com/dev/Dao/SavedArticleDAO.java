4b4c3b3f5b7105f3da72b09d475a71d108fbd056
Hao <truonggiahao45@gmail.com>
2024-11-18 23:45:34 +0700

    new feature in main page

---
+++ DevConnect/src/main/java/com/dev/Dao/SavedArticleDAO.java
@@ -0,0 +1,17 @@
+package com.dev.Dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dev.Model.SavedArticle;
+import com.dev.Model.SavedArticleKey;
+import com.dev.Model.User;
+
+@Repository
+public interface SavedArticleDAO extends JpaRepository<SavedArticle, SavedArticleKey> {
+
+	List<SavedArticle> findByUser(User user);
+	
+}	
---
+++ DevConnect/src/main/java/com/dev/Model/SavedArticle.java
@@ -0,0 +1,44 @@
+package com.dev.Model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="saved_articles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SavedArticle {
+
+	@EmbeddedId
+	SavedArticleKey id;
+	
+	@ManyToOne
+	@MapsId("user_id")
+	@JoinColumn(name="user_id")
+	User user;
+	
+	@ManyToOne
+	@MapsId("article_id")
+	@JoinColumn(name="article_id")
+	Article article;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	Date save_date;
+	
+	@Column(name="read")
+	int read;
+	
+}
