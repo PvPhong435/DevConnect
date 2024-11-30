e6f1dbe86dc60a804b0f2e9980800c44f8b9c092
TrongTin <trongtin.tranthai@gmail.com>
2024-11-30 14:12:52 +0700

    Try to fix duplicated homeController

--- DevConnect/src/main/java/com/dev/Controller/homeController.java
+++ DevConnect/src/main/java/com/dev/Controller/homeController.java
@@ -2,7 +2,6 @@ package com.dev.Controller;
 
 import java.util.List;
 
-import com.dev.Util.SlugUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,20 +11,19 @@ import com.dev.Model.Article;
 
 @Controller
 public class homeController {
-	
-	ArticleDAO articleDAO;
 
-	public homeController(ArticleDAO articleDAO) {
-		super();
-		this.articleDAO = articleDAO;
-	}
+    ArticleDAO articleDAO;
 
-	@GetMapping({"/home/index", "/"})
-	public String getHomePage(Model model) {
-		List<Article> list= articleDAO.findAll();
-		model.addAttribute("list", list);
-		return "home/index";
-	}
-	
-	
+    public homeController(ArticleDAO articleDAO) {
+        super();
+        this.articleDAO = articleDAO;
+    }
+
+    @GetMapping({"/home/index", "/"})
+    public String getHomePage(Model model) {
+        List<Article> list= articleDAO.findAll();
+        model.addAttribute("list", list);
+        return "home/index";
+    }
+
 }
